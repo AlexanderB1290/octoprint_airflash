91dc63e2d2b0d332a737eded4fdba08508289e63
Aleksandar Bushev <alexander.bushev1290@gmail.com>
2019-04-26 10:00:18 +0300

    clean up unused fragments

--- airflash/__init__.py
+++ airflash/__init__.py
@@ -1,29 +1,12 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
 class AirflashPlugin(octoprint.plugin.StartupPlugin,
 					 octoprint.plugin.TemplatePlugin,
 					 octoprint.plugin.SettingsPlugin,
                      octoprint.plugin.AssetPlugin):
-
-	# noinspection PyMissingConstructor
-	def __init__(self):
-		self._ip_address = 'flashair'
-		self._upload_path = '/upload.cgi'
-
-	def initialize(self):
-		self._ip_address = self._settings.get(["ip_address"])
-		self._upload_path = self._settings.get(["upload_path"])
 
 	##~~ StartupPlugin mixin
 
@@ -44,22 +27,21 @@ class AirflashPlugin(octoprint.plugin.StartupPlugin,
 			)
 		)
 
+	def on_settings_save(self, data):
+		# For debug purpose only
+		self._logger.info("New data: {data}".format(**locals()))
+		octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+
+	##~~ TemplatePlugin and AssetPlugin mixin
+
 	def get_template_configs(self):
 		return [
 			dict(type="settings", custom_bindings=False)
 		]
 
-	def on_settings_save(self, data):
-		self._logger.info("New data: {data}".format(**locals()))
-		octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
-
 	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
 		return dict(
-			js=["js/airflash.js"],
-			css=["css/airflash.css"],
-			less=["less/airflash.less"]
+			js=["js/airflash.js"]
 		)
 
 	##~~ Softwareupdate hook
@@ -85,9 +67,6 @@ class AirflashPlugin(octoprint.plugin.StartupPlugin,
 		)
 
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "AirFlash Uploader"
 
 def __plugin_load__():
